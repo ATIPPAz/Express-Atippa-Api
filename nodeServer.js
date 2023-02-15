2b7b4dc9d8fc29a797c6fc0c7da933a15d6781f0
Atip Nasakun <71032885+ATIPPAz@users.noreply.github.com>
2023-02-15 13:07:35 +0700

    Add files via upload

---
+++ nodeServer.js
@@ -0,0 +1,18 @@
+const express = require('express')
+var cors = require('cors')
+const app =  express()
+app.use(cors())
+const port = 5000
+app.get("/api",(req,res)=>{
+  // res.send(200, 'hi')
+  // res.json({ a: 2 })
+  res.status(200).send({ a: 5555 })
+})
+app.get("/",(req,res)=>{
+  // res.send(200, 'hi')
+  // res.json({ a: 2 })
+  res.status(200).send({ a: 2 })
+})
+app.listen(port,()=>{
+  console.log('listening at ' , port)
+})
